53d2843d8ab0bc526bbf69da5677d141d348fc20
Esau A. Hervert Hernandez <eah170630@utdallas.edu>
2023-08-13 13:03:17 -0500

    2023-08-13 -- Added cuda for remote usage of GPU

--- Benchmarks/train_dhdn.py
+++ Benchmarks/train_dhdn.py
@@ -2,16 +2,25 @@ import os
 import dataset
 import dhdn
 import time
+import torch
 from torch.utils.data import DataLoader
 
 path = os.getcwd() + '/instances/sidd_instances_064.csv'
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 SIDD = dataset.DatasetSIDD(csv_file=path, transform=dataset.RandomProcessing())
-dataloader_sidd = DataLoader(dataset=SIDD, batch_size=16, shuffle=True, num_workers=0)
+if torch.cuda.is_available():
+    dataloader_sidd = DataLoader(dataset=SIDD, batch_size=128, shuffle=True, num_workers=16)
+else:
+    dataloader_sidd = DataLoader(dataset=SIDD, batch_size=16, shuffle=True, num_workers=0)
+
 net = dhdn.Net()
+net.to(device)
 
 for i_batch, sample_batch in enumerate(dataloader_sidd):
     time0 = time.time()
     print(i_batch, sample_batch['NOISY'].size(), sample_batch['GT'].size())
-    print(net(sample_batch['NOISY']).size())
+    x = sample_batch['NOISY'].to(device)
+    print(net(x).size())
     print(time.time() - time0)
