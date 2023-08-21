01060213bb045abdf48bc8f6d383ad56f92e0eb7
Esau A. Hervert Hernandez <eah170630@utdallas.edu>
2023-08-21 13:11:44 -0500

    2023-08-21 -- Changed learning rate from 1e-3 to 1e-2

--- Benchmarks/train_dhdn.py
+++ Benchmarks/train_dhdn.py
@@ -20,7 +20,7 @@ else:
 net = dhdn.Net()
 net.to(device)
 
-optim = torch.optim.Adam(net.parameters(), lr=1e-3)
+optim = torch.optim.Adam(net.parameters())
 loss = torch.nn.L1Loss()
 
 for epoch in range(EPOCHS):
