2f35302aee284f9c2c9a649b3f9406338265ff58
Esau A. Hervert Hernandez <eah170630@utdallas.edu>
2023-10-12 17:47:15 -0500

    2023-10-12 -- Added Generator and Discriminator for CycleGAN exploration

---
+++ ENAS-CycleGAN/README.md
@@ -0,0 +1,3 @@
+Code pulled from:
+
+    https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
---
+++ ENAS-CycleGAN/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ENAS_CycleGAN',
+      version='0.0.1',
+      description='CycleGAN of the ENAS Networks',
+      author='Esau Hervert Hernandez',
+      author_email='eah170630@utdallas.edu'
+      )
