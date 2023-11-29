4cdb7d2c0eaab14983c6744ca502712437306a92
Esau A. Hervert Hernandez <eah170630@utdallas.edu>
2023-11-29 16:39:21 -0600

    2023-11-29 -- Improvements to Search functions

--- ENAS-DHDN/setup.py
+++ ENAS-DHDN/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ENAS_DHDN',
-      version='3.3.2',
+      version='3.3.3',
       description='DHDN component of the ENAS Networks',
       author='Esau Hervert Hernandez',
       author_email='eah170630@utdallas.edu'
