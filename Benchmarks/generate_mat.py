974b188799c5311caf87b56ed6b581aee67603f6
Esau A. Hervert Hernandez <eah170630@utdallas.edu>
2024-04-26 11:40:32 -0500

    2024-04-26 -- generate_mat now generates metrics

--- Benchmarks/generate_mat.py
+++ Benchmarks/generate_mat.py
@@ -2,6 +2,7 @@ import os
 import copy
 import datetime
 import json
+import pandas as pd
 from scipy.io import loadmat, savemat
 import numpy as np
 import torch
@@ -9,7 +10,8 @@ from ENAS_DHDN import SHARED_DHDN as DHDN
 import srgb_conc_unet
 import utilities.dataset as dataset
 from torch.utils.data import DataLoader
-from utilities.functions import list_of_ints, get_out
+from utilities.functions import list_of_ints, get_out, SSIM
+from utilities.functions import True_PSNR as PSNR
 import argparse
 
 current_time = datetime.datetime.now()
@@ -30,6 +32,8 @@ parser.add_argument('--decoder', default=[0, 0, 0, 0, 0, 0, 0, 0, 0], type=list_
 parser.add_argument('--channels', default=128, type=int)  # Channel Number for DHDN
 parser.add_argument('--k_value', default=3, type=int)  # Size of encoder and decoder
 parser.add_argument('--model_file', default='models/model.pth', type=str)  # Model path to weights
+parser.add_argument('--generate_metrics', default=False, type=lambda s: (str(s).lower() == 'true'))  # Generate Metrics
+parser.add_argument('--generate_mat', default=True, type=lambda s: (str(s).lower() == 'true'))  # Generate Metrics
 # To use self-ensembling
 args = parser.parse_args()
 
@@ -43,14 +47,17 @@ def main():
 
     # Load benchmark data for processing
     if args.type == 'validation':
-        mat_file = dir_current + config['Locations'][args.dataset]['Validation_Noisy']
+        mat_gt_file = dir_current + config['Locations'][args.dataset]['Validation_GT']
+        mat_noisy_file = dir_current + config['Locations'][args.dataset]['Validation_Noisy']
     else:
         if args.dataset == 'SIDD':
-            mat_file = dir_current + config['Locations'][args.dataset]['Benchmark']
+            mat_gt_file = None
+            mat_noisy_file = dir_current + config['Locations'][args.dataset]['Validation_Noisy']
         else:
-            mat_file = dir_current + config['Locations'][args.dataset]['Testing']
+            mat_gt_file = dir_current + config['Locations'][args.dataset]['Testing_GT']
+            mat_noisy_file = dir_current + config['Locations'][args.dataset]['Validation_Noisy']
 
-    mat = loadmat(mat_file)
+    mat_noisy = loadmat(mat_noisy_file)
 
     # Get the model paths
     model_dhdn = dir_current + '/' + args.model_file
@@ -93,7 +100,7 @@ def main():
 
     dhdn.load_state_dict(state_dict_dhdn)
 
-    SIDD_validation = dataset.DatasetMAT(mat_noisy_file=mat_file, mat_gt_file=None, device=device_0)
+    SIDD_validation = dataset.DatasetMAT(mat_noisy_file=mat_noisy_file, mat_gt_file=mat_gt_file, device=device_0)
     if args.dataset == 'SIDD':
         dataloader_sidd_validation = DataLoader(dataset=SIDD_validation,
                                                 batch_size=config['Training']['Testing_Batch_Size'],
@@ -104,34 +111,81 @@ def main():
                                                 shuffle=False)
 
     y_dhdn_final = []
+    dict_out = []
+    ssim_base = 0
+    psnr_base = 0
+    ssim_denoise_val = 0
+    psnr_denoise_val = 0
+    count = 0
     for i_batch, sample_batch in enumerate(dataloader_sidd_validation):
-        x_sample_pt = sample_batch['NOISY']
+        x_noisy_pt = sample_batch['NOISY']
+
         with torch.no_grad():
-            y_dhdn = dhdn(x_sample_pt.to(device_0))
+            y_out_pt = dhdn(x_noisy_pt.to(device_0))
+
             print('Batch {i} processed.'.format(i=i_batch))
-            y_dhdn_final.append(get_out(y_dhdn))
+            y_dhdn_final.append(get_out(y_out_pt))
 
-        del x_sample_pt, y_dhdn
+        if mat_gt_file is not None and args.generate_metrics:
+            x_gt_pt = sample_batch['GT']
 
-    if not os.path.exists(
-            dir_current + '/results/{type}/{name}/'.format(name=args.name, type=args.type)):
-        os.makedirs(dir_current + '/results/{type}/{name}/'.format(name=args.name, type=args.type))
+            ssim_denoise = SSIM(x_gt_pt, y_out_pt)
+            psnr_denoise = PSNR(x_gt_pt, y_out_pt)
 
-    if dataset == 'SIDD':
-        validation_tag = 'ValidationNoisyBlocksSrgb'
-        benchmark_tag = 'BenchmarkNoisyBlocksSrgb'
-    else:
-        validation_tag = 'val_ng'
-        benchmark_tag = 'test_ng'
+            ssim = SSIM(x_gt_pt, x_noisy_pt)
+            psnr = PSNR(x_gt_pt, x_noisy_pt)
 
-    y_dhdn_final = np.array(y_dhdn_final, dtype=np.uint8)
-    file_dhdn = 'results/{type}/{name}/SubmitSrgb.mat'.format(name=args.name, type=args.type)
-    mat_dhdn = copy.deepcopy(mat)
-    if args.type == 'validation':
-        mat_dhdn[validation_tag] = y_dhdn_final
-    else:
-        mat_dhdn[benchmark_tag] = y_dhdn_final
-    savemat(file_dhdn, mat_dhdn)
+            dict_item = {'Image': i_batch, 'Denoised_PSNR': round(ssim_denoise.item(), 6),
+                         'Denoised_SSIM': round(psnr_denoise.item(), 6),
+                         'Base_PSNR': round(ssim.item(), 6), 'Base_SSIM': round(psnr.item(), 6)}
+            print(dict_item)
+            dict_out.append(dict_item)
+
+            ssim_base += ssim.item()
+            psnr_base += psnr.item()
+            ssim_denoise_val += ssim_denoise.item()
+            psnr_denoise_val += psnr_denoise.item()
+            count += 1
+            del x_gt_pt
+
+        del x_noisy_pt, y_out_pt
+
+    if args.generate_metrics:
+        ssim_base /= count
+        psnr_base /= count
+        ssim_denoise_val /= count
+        psnr_denoise_val /= count
+
+        dict_item = {'Image': 'ALL', 'Denoised_PSNR': round(ssim_denoise_val, 6),
+                     'Denoised_SSIM': round(psnr_denoise_val, 6),
+                     'Base_PSNR': round(ssim_base, 6), 'Base_SSIM': round(psnr_base, 6)}
+
+        print(dict_item)
+        dict_out.append(dict_item)
+
+        dataframe_out = pd.DataFrame(dict_out)
+        dataframe_out.to_csv('csv_out/' + args.name + '.csv', index=False)
+
+    if args.generate_mat:
+        if not os.path.exists(
+                dir_current + '/results/{type}/{name}/'.format(name=args.name, type=args.type)):
+            os.makedirs(dir_current + '/results/{type}/{name}/'.format(name=args.name, type=args.type))
+
+        if dataset == 'SIDD':
+            validation_tag = 'ValidationNoisyBlocksSrgb'
+            benchmark_tag = 'BenchmarkNoisyBlocksSrgb'
+        else:
+            validation_tag = 'val_ng'
+            benchmark_tag = 'test_ng'
+
+        y_dhdn_final = np.array(y_dhdn_final, dtype=np.uint8)
+        file_dhdn = 'results/{type}/{name}/SubmitSrgb.mat'.format(name=args.name, type=args.type)
+        mat_dhdn = copy.deepcopy(mat_noisy)
+        if args.type == 'validation':
+            mat_dhdn[validation_tag] = y_dhdn_final
+        else:
+            mat_dhdn[benchmark_tag] = y_dhdn_final
+        savemat(file_dhdn, mat_dhdn)
 
 
 if __name__ == "__main__":
